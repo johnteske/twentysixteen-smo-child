3a3eac2bb0bcbd27f7a51e47af7d616bf9491cc7
johnteske <john.teske@gmail.com>
2016-10-06 21:07:33 -0700

    Increase custom logo size to 480px
    Issue #3
    
    This should help resolve the fuzzy appearance on mobile

--- functions.php
+++ functions.php
@@ -23,3 +23,13 @@ function smo_widgets_init() {
 
 }
 add_action( 'widgets_init', 'smo_widgets_init' );
+
+
+add_action( 'after_setup_theme', 'child_custom_logo_setup', 11 );
+function child_custom_logo_setup() {
+	add_theme_support( 'custom-logo', array(
+		'height'      => 480,
+		'width'       => 480,
+		'flex-height' => true,
+	));
+}
