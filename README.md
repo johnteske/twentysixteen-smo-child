dc5459ebd8aae7ecb21def93622fb7719366db85
johnteske <john.teske@gmail.com>
2016-10-09 21:33:02 -0700

    Add [smo_button] shortcode

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Compile styles
 
 `sass --watch scss/style.scss:style.css`
+
+`[smo_button url="#"]Button Text[/smo_button]`
--- functions.php
+++ functions.php
@@ -53,3 +53,21 @@ function child_custom_logo_setup() {
 		'flex-height' => true,
 	));
 }
+
+// Add [smo_button] Shortcode
+function smo_button_shortcode( $atts , $content = null ) {
+
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'url' => '#',
+		),
+		$atts,
+		'smo_button'
+	);
+
+	// Return image HTML code
+	return '<a href="' . $atts['url'] . '" class="smo-button">' . $content . '</a>';
+
+}
+add_shortcode( 'smo_button', 'smo_button_shortcode' );
