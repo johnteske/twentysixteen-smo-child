7e3e9c5bff35cb40889d1870575b7e7ab87e0cb2
johnteske <john.teske@gmail.com>
2016-10-10 21:57:41 -0700

    Add basic widget to list upcoming concerts
    Issue #5

--- functions.php
+++ functions.php
@@ -71,3 +71,81 @@ function smo_button_shortcode( $atts , $content = null ) {
 
 }
 add_shortcode( 'smo_button', 'smo_button_shortcode' );
+
+
+
+
+//
+// custom SMO event list widget
+//
+add_action( 'widgets_init', function(){
+    register_widget( 'smo_event_list' );
+});
+
+class smo_event_list extends WP_Widget {
+
+    /**
+     * Sets up the widgets name etc
+     */
+    public function __construct() {
+        $widget_ops = array(
+            'classname' => 'smo-event-list',
+            'description' => '',
+        );
+
+        parent::__construct( 'smo_event_list', 'SMO Event List', $widget_ops );
+    }
+
+
+    /**
+     * Outputs the content of the widget
+     *
+     * @param array $args
+     * @param array $instance
+     */
+    public function widget( $args, $instance ) {
+        // outputs the content of the widget
+        echo $args['before_widget'];
+        if ( ! empty( $instance['title'] ) ) {
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+        }
+        include 'smo-event-list.php';
+        // echo __( 'Hello, World!', 'text_domain' );
+        echo $args['after_widget'];
+    }
+
+
+    /**
+     * Outputs the options form on admin
+     *
+     * @param array $instance The widget options
+     */
+    public function form( $instance ) {
+        // outputs the options form on admin
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Upcoming Concerts', 'text_domain' );
+        ?>
+            <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+            </p>
+                        
+        <?php
+    }
+
+
+    /**
+     * Processing widget options on save
+     *
+     * @param array $new_instance The new options
+     * @param array $old_instance The previous options
+     */
+    public function update( $new_instance, $old_instance ) {
+        // processes widget options to be saved
+        foreach( $new_instance as $key => $value )
+        {
+            $updated_instance[$key] = sanitize_text_field($value);
+        }
+
+        return $updated_instance;
+    }
+}
