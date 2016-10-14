467ef7db00ddcf831d7c24fcaa640d99e36de1ff
johnteske <john.teske@gmail.com>
2016-10-13 22:25:03 -0700

    Add TinyMCE formats for callout and buttons

--- functions.php
+++ functions.php
@@ -128,7 +128,7 @@ class smo_event_list extends WP_Widget {
             <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
             </p>
-                        
+
         <?php
     }
 
@@ -149,3 +149,44 @@ class smo_event_list extends WP_Widget {
         return $updated_instance;
     }
 }
+
+
+
+//
+// Add custom styles to TinyMCE
+//
+
+// Callback function to insert 'styleselect' into the $buttons array
+function my_mce_buttons_2( $buttons ) {
+	array_unshift( $buttons, 'styleselect' );
+	return $buttons;
+}
+// Register our callback to the appropriate filter
+add_filter( 'mce_buttons_2', 'my_mce_buttons_2' );
+// Callback function to filter the MCE settings
+function my_mce_before_init_insert_formats( $init_array ) {
+	// Define the style_formats array
+	$style_formats = array(
+		// Each array child is a format with it's own settings
+		array(
+			'title' => 'SMO callout',
+			'block' => 'div',
+			'classes' => 'smo-callout',
+			'wrapper' => true,
+
+		),
+        array(
+	        'title' => 'SMO button (apply to a link)',
+	        'selector' => 'a',
+	        'classes' => 'smo-button'
+        ),
+
+	);
+	// Insert the array, JSON ENCODED, into 'style_formats'
+	$init_array['style_formats'] = json_encode( $style_formats );
+
+	return $init_array;
+
+}
+// Attach callback to 'tiny_mce_before_init'
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
